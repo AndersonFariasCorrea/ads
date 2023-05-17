e094b310c544bea96cbb8aac481d29ec67f3f9d5
AndersonFariasCorrea <deidaraa2002@gmail.com>
2023-05-17 00:38:17 -0300

    gen pass almost final

--- gen_pass/library.py
+++ gen_pass/library.py
@@ -1,19 +1,6 @@
-lowercase_letters = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'á', 'à', 'â', 'ã', 'é', 'è', 'ê', 'í', 'ì', 'î', 'ó', 'ò', 'ô', 'õ', 'ú', 'ù', 'û', 'ç')
-
-uppercase_letters = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Á', 'À', 'Â', 'Ã', 'É', 'È', 'Ê', 'Í', 'Ì', 'Î', 'Ó', 'Ò', 'Ô', 'Õ', 'Ú', 'Ù', 'Û', 'Ç')
-
-numbers = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-
-special_characters = ('!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', '\\', '/', '<', '>', ',', '.', ';', ':', '?')
-
 def parse_string_to_int(string):
     try:
-        result = None
         number = int(string)
-        if number >= 6 and number <= 16:
-            result = number, True
-        else:
-            result = None, False
-        return result, 
+        return number, True
     except ValueError:
         return None, False
--- gen_pass/main.py
+++ gen_pass/main.py
@@ -3,18 +3,32 @@ import time
 
 pass_length = library.parse_string_to_int(input("informe o tamanho da senha:\t"))
 
-print(pass_length)
-
-string_number = str(time.time())
-
-# Find the index of the decimal point
-decimal_index = string_number.index('.')
-
-# Extract the substring after the decimal point
-numbers_after_decimal = string_number[decimal_index + 1:]
-
 while pass_length[1] is False:
     pass_length = library.parse_string_to_int(input("informe o tamanho da senha:\t"))
-    print(pass_length)
 
-print(numbers_after_decimal)
+data = {
+    'a1': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'á', 'à', 'â', 'ã', 'é', 'è', 'ê', 'í', 'ì', 'î', 'ó', 'ò', 'ô', 'õ', 'ú', 'ù', 'û', 'ç'],
+    'a2': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Á', 'À', 'Â', 'Ã', 'É', 'È', 'Ê', 'Í', 'Ì', 'Î', 'Ó', 'Ò', 'Ô', 'Õ', 'Ú', 'Ù', 'Û', 'Ç'],
+    'a3': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    'a4': ['!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', '\\', '/', '<', '>', ',', '.', ';', ':', '?']
+}
+
+def generatePassword(length):
+    timestamp = str(int(time.time() * 1000))
+    password = ''
+
+    for i in range(length):
+        char = timestamp[i % len(timestamp)]
+        index = int(char)
+
+        array = data['a' + str(index % 4 + 1)]
+
+        elementIndex = index % len(array)
+        element = array[elementIndex]
+
+        password += element
+
+    return password
+
+password = generatePassword(pass_length[0])
+print(password)
