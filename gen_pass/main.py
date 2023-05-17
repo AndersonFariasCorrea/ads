c20aad08aa278e8382db2ea72611ca441d58dfa8
AndersonFariasCorrea <deidaraa2002@gmail.com>
2023-05-16 22:51:34 -0300

    second pass gen commit

--- gen_pass/main.py
+++ gen_pass/main.py
@@ -1,9 +1,11 @@
 import library
 import time
 
-string_number = str(time.time())
 
-length = library.parse_string_to_int(input("informe o tamanho da senha:\t"))
+
+pass_length = library.parse_string_to_int(input("informe o tamanho da senha:\t"))
+
+string_number = str(time.time())
 
 # Find the index of the decimal point
 decimal_index = string_number.index('.')
@@ -11,5 +13,5 @@ decimal_index = string_number.index('.')
 # Extract the substring after the decimal point
 numbers_after_decimal = string_number[decimal_index + 1:]
 
-while length[1] is False and length[0] is not None and length[0] > 0:
-    a = 0
+while pass_length[1] is False and pass_length[0] is not None and pass_length[0] > 0:
+    pass_length = library.parse_string_to_int(input("informe o tamanho da senha:\t"))
