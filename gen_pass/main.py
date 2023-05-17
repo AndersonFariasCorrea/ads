01c68acb56357a86ea0a90cbdca7edd79c2b9646
AndersonFariasCorrea <deidaraa2002@gmail.com>
2023-05-16 22:58:18 -0300

    length size setted

--- gen_pass/main.py
+++ gen_pass/main.py
@@ -13,5 +13,6 @@ decimal_index = string_number.index('.')
 # Extract the substring after the decimal point
 numbers_after_decimal = string_number[decimal_index + 1:]
 
-while pass_length[1] is False and pass_length[0] is not None and pass_length[0] > 0:
-    pass_length = library.parse_string_to_int(input("informe o tamanho da senha:\t"))
+while pass_length[1] is False and pass_length[0] is not None:
+    if  pass_length[0] > 6 and pass_length[0] < 16:
+        pass_length = library.parse_string_to_int(input("informe o tamanho da senha:\t"))
